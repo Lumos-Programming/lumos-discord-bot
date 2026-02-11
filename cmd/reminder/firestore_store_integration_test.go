df158667244296cbedaa44cf9d3c154d87654e64
Shion Ichikawa <shion1305@gmail.com>
2026-02-11 22:01:28 +0900

    👷 (ci) Run Firestore integration tests

--- cmd/reminder/firestore_store_integration_test.go
+++ cmd/reminder/firestore_store_integration_test.go
@@ -50,7 +50,7 @@ func TestFirestoreReminderStore_EmulatorLifecycle(t *testing.T) {
 		executed:    false,
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	if err := store.UpsertDraft(ctx, id, info); err != nil {
